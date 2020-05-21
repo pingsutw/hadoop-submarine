4b9039ed4088481d60eab1742cfec98a46538c58
pingsutw <pingsutw@gmail.com>
2020-05-21 18:43:58 +0800

    WIP

--- submarine-sdk/pysubmarine/submarine/job/api/__init__.py
+++ submarine-sdk/pysubmarine/submarine/job/api/__init__.py
@@ -13,27 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
 
-from submarine.job import ApiClient, JobLibrarySpec, JobSpec, JobTaskSpec, ApiClient,\
-    Configuration, JobsApi
+# flake8: noqa
 
-import submarine.tracking.fluent
-import submarine.tracking as tracking
-
-log_param = submarine.tracking.fluent.log_param
-log_metric = submarine.tracking.fluent.log_metric
-set_tracking_uri = tracking.set_tracking_uri
-get_tracking_uri = tracking.get_tracking_uri
-
-
-__all__ = ["log_metric",
-           "log_param",
-           "set_tracking_uri",
-           "get_tracking_uri",
-           "ApiClient",
-           "JobLibrarySpec",
-           "JobSpec",
-           "JobTaskSpec",
-           "Configuration",
-           "JobsApi"
-           ]
+# import apis into api package
+from submarine.job.api.jobs_api import JobsApi
